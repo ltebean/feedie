48d8a0dc63cf58966ebe711a2d7c424cd47b51b1
ltebean <yucong1118@gmail.com>
2014-06-23 17:50:46 +0800

    init

---
+++ test/index.js
@@ -0,0 +1,31 @@
+var feed = require('../').init({
+    host: '127.0.0.1',
+    port: '6379',
+    options: {},
+    namespace: 'usergraph'
+});
+
+var graph = feed.graph;
+
+
+graph.follow('ltebean', 'kael', function(err, res) {})
+graph.follow('ltebean', 'spud', function(err, res) {})
+
+
+feed.broadcastBy('kael', {
+    action: 'new project',
+    detail: 'add a package called blabla'
+}, function() {})
+
+feed.sendTo('ltebean', {
+    action: 'follow',
+    detail: 'some one follows you'
+}, function() {})
+
+feed.load('ltebean', 0, 10, function(err, res) {
+    console.log("ltebean's feed: %s", JSON.stringify(res))
+});
+
+feed.load('spud', 0, 10, function(err, res) {
+    console.log("spud's feed: %s", JSON.stringify(res))
+});
